c51e7c7982d6573eb31ba29cedc6bde4917ba8c2
caglartoklu <caglartoklu@gmail.com>
2019-05-24 21:41:40 +0300

    copying accompanying files after build.

--- make_build.bat
+++ make_build.bat
@@ -2,3 +2,6 @@ CD %~dp0
 REM more options: https://pyinstaller.readthedocs.io/en/latest/usage.html
 REM pyinstaller --console --onedir bashere.py
 python -OO -m PyInstaller --console --onedir bashere.py
+copy README.md dist\bashere
+copy LICENSE.txt dist\bashere
+copy bashere.py dist\bashere
